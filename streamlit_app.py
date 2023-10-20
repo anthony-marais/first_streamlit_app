1b6b60cbe143cae5dc808ef333c53fa4a5edf570
anthony-marais <67336192+anthony-marais@users.noreply.github.com>
2023-10-20 10:51:43 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,8 +36,6 @@ try:
     # write your own comment -what does the next line do? 
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
     # write your own comment - what does this do?
-    streamlit.write('The user entered ', fruit_choice)
-
     streamlit.dataframe(fruityvice_normalized)
     
 except ULRError as e:
