bc80e90838b5e59a8a634d633aab505139e9a89c
anthony-marais <67336192+anthony-marais@users.noreply.github.com>
2023-10-20 10:30:06 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,4 +57,4 @@ streamlit.write('Thanks for adding ', add_my_fruit)
 
 
 
-my_cur.execute('insert into pc_rivery_db.public.fruit_load_list values ('from streamlit')')
+my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values ('from streamlit')")
