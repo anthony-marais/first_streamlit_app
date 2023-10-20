29932d7e6fdb8e10ccdbc3bf11cad78ce729a931
anthony-marais <67336192+anthony-marais@users.noreply.github.com>
2023-10-20 10:56:22 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,14 +50,6 @@ except ULRError as e:
 
 
 
-
-
-# write your own comment -what does the next line do? 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
-streamlit.dataframe(fruityvice_normalized)
-
-
 streamlit.stop()
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
